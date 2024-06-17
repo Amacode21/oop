d07b1a75cfe5be7e8ae329658e623470a2ae4481
= <josedalanon630@gmail.com>
2024-06-17 16:58:06 +0800

    Change the filename

--- main.py
+++ main.py
@@ -1,44 +1,106 @@
 class Person():
-    def __init__(self, name:str = '', age:int = 0, gender:str = '' ):
+    def __init__(self, name:str, age:int) -> None:
         self.name = name
-        self.age = age
-        self.gender = gender
-    
-    def walk(self):
-        return 'Im walking!'
-    
-    def __str__(self):
-        return f'Hello my name is {self.name} im {self.age} years old : {self.gender}'
+        self.age = age 
 
+    def __str__(self):
+        return f'Hi! my name is {self.name} and i am {self.age} years old!'
 
 class Student(Person):
-    def __init__(self, name, age,gender, grade) -> None:
-        super().__init__(name, age, gender)
-        self.grade = grade
+    def __init__(self, name:str, age:int, student_id:int) -> None:
+        super().__init__(name, age)
+        self.student_id = student_id
+        self.courses = set()
 
-    def __iter__(self):
-        self.n = 1
-        return self
+    def enroll_course(self, course_name:str) -> None:
+        self.courses.add(course_name)
+        
+
+    def drop_course(self, course_name:str) -> None:
+        if course_name in self.courses:
+            self.courses.remove(course_name)
+            return
+        print('Course cant find')      
+
+    def get_details(self):
+        print(f'Name: {self.name}')
+        print(f'Age: {self.age}')
+        print(f'ID: {self.student_id}')
+        print(f'Courses Currently Enrolled')
+        print(f'--------------------------')
+        if len(self.courses) < 1:
+            print('No courses enrolled')
+            return
+        for _, course in enumerate(self.courses, 1):
+            print(f'{_}. {course}')
+class Professor(Person):
+    def __init__(self, name:str, age:int, employee_id:int, department:str) -> None:
+        super().__init__(name, age)
+        self.employee_id = employee_id
+        self.department = department
+
+    def assign_grade(self, student_id:int, course_name:'Course', grade: str) -> None:
+        for student in course_name.students_enrolled:
+            if(student['student_id'] == student_id):
+                student['grade'] = grade
+                return
+class Course:
+    def __init__(self, course_name:str, professor: Professor) -> None:
+        self.course_name = course_name
+        self.professor = professor
+        self.students_enrolled = []
     
-    def __next__(self):
-        if(self.n <= 20):
-            x = self.n
-            self.n += 1
-            return x
-        else:
-            raise StopIteration
+    def display_students(self) -> None:
+        print(f'---{self.course_name}---')
+        print('List of Students Enrolled')
+        print('------------------------')
+        for student in self.students_enrolled:
+            for k, v in student.items():
+                print(f'{k}: {v}', end=' ')
+            print('\n------------------------')
 
-    def __str__(self):
-        return f'Hello my name is {self.name} im {self.age} years old i am grade {self.grade}'
+    def enroll_student(self, student_id:int) -> None:
+        for students in self.students_enrolled:
+            if student_id == students['student_id']:
+                print(f'This student is already enrolled')
+                return
+            
+        new_student:dict = {
+                     'student_id': student_id, 
+                     'grade': None
+                    }
+        self.students_enrolled.append(new_student)
+        print('Student Enrolled')
 
+    def remove_student(self, student_id:int) -> None:
+        for index, students  in enumerate(self.students_enrolled):
+            if student_id == students['student_id']:
+                self.students_enrolled.pop(index)
+                print('Student removed')
+                return
 
+    def __str__(self) -> str:
+        self.x = f'Course: {self.course_name}\nProfessor: {self.professor.name}'
+        return self.x
 
+        
+def main() -> None:
+    student_1 = Student('Jose Dalanon',21,1)
+    student_2 = Student('Maria Magalang',21,2)
+    professor_1 = Professor('James William', 31, 1, 'CIT')
+    professor_2 = Professor('Mike James', 45, 2, 'CIT')
+    course_1 = Course('Intro to Web Development', professor_1)
+    course_2 = Course('Introduction to Python', professor_2)
 
-def main():
-    new_person = Student('Jose',21,'Male',6)
-    print(new_person)
-    print(new_person.walk())
-    new_person.circle(4)
+    course_1.enroll_student(student_1.student_id)
+    student_1.enroll_course(course_1.course_name)
+
+    course_1.enroll_student(student_2.student_id)
+    student_2.enroll_course(course_1.course_name)
+    
+    professor_1.assign_grade(1,course_1,'A')
+    professor_1.assign_grade(5,course_1,'B')
+    course_1.display_students()
 
 if __name__ == "__main__":
     main()
